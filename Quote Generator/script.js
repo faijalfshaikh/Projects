a8d32a25870886e508b43cee810d5d26796a5fa0
faijalfshaikh <faijalfshaikh@gmail.com>
2025-09-20 23:49:08 +0530

    Finalised Project with working API

--- Quote Generator/script.js
+++ Quote Generator/script.js
@@ -1,1 +1,18 @@
-console.log("Hello World")
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+
+const url = "https://quotes-api-self.vercel.app/quote"
+
+async function getQuote(url){
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    quote.innerHTML = data.quote;
+    author.innerHTML = data.author;
+}
+
+getQuote(url);
+
+function tweet(){
+    window.open("https://twitter.com/intent/tweet?text=" + quote.innerHTML + "by-------" + author.innerHTML,"Tweet Window","width=300 height=300")
+}
